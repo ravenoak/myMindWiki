e6aa73cf7487097b457383330439919b3ca08715
RavenOak <ravenoak@gmail.com>
2022-06-24 02:07:41 -0700

    Switches over to using django-markdownx

--- mymindwiki/settings.py
+++ mymindwiki/settings.py
@@ -36,12 +36,14 @@ try:
     DATA_DIR = Path(env("DATA_DIR"))
     DATABASE_DIR = DATA_DIR / "db"
     MEDIA_ROOT = DATA_DIR / "media"
+    STATIC_ROOT = DATA_DIR / "static"
 except Exception as e:
     logger.debug(
         "Environment variable 'DATA_DIR' not set, looking for 'DATABASE_DIR' "
         "and 'MEDIA_DIR' instead.")
     DATABASE_DIR = Path(env("DATABASE_DIR"))
     MEDIA_ROOT = Path(env("MEDIA_DIR"))
+    STATIC_ROOT = Path(env("STATIC_DIR"))
 
 DEBUG = env('DEBUG')
 SECRET_KEY = env("SECRET_KEY")
@@ -52,6 +54,15 @@ if DEBUG is False and SECRET_KEY == 'DEV-ENV-ONLY':
     raise ImproperlyConfigured
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+
+SITE_HOSTNAME = env('SITE_HOSTNAME')
+SITE_PORT = env('SITE_PORT')
+SSL_ENABLED = env('SSL_ENABLED')
+
+XPORT = 'http://'
+if SSL_ENABLED:
+    XPORT = 'https://'
+EXTERNAL_URL = f"{XPORT}{SITE_HOSTNAME}:{SITE_PORT}"
 
 # Application definition
 
@@ -64,6 +75,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'markdownx',
 ]
 
 MIDDLEWARE = [
@@ -96,10 +108,33 @@ TEMPLATES = [
     },
 ]
 
+MARKDOWNX_MARKDOWN_EXTENSIONS = [
+    'codehilite',
+    'fenced_code',
+    'tables',
+    'toc',
+    'wikilinks',
+    'mindwiki.markdown_ext.ext_links',
+    'mindwiki.markdown_ext.snippets',
+]
+MARKDOWNX_MARKDOWN_EXTENSION_CONFIGS = {
+    'wikilinks': {
+        'base_url': '/wiki/page/'
+    },
+    'mindwiki.markdown_ext.ext_links': {
+        'base_url': EXTERNAL_URL + '/wiki/weblink/',
+        'end_url': '/?json=true',
+    },
+    'mindwiki.markdown_ext.snippets': {
+        'base_url': EXTERNAL_URL + '/wiki/snippet/',
+        'end_url': '/?json=true',
+    },
+}
+
 WSGI_APPLICATION = 'mymindwiki.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -109,7 +144,7 @@ DATABASES = {
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -131,7 +166,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -144,7 +179,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
@@ -152,12 +187,10 @@ STATICFILES_DIRS = [
 ]
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 
-SITE_HOSTNAME = env('SITE_HOSTNAME')
-SITE_PORT = env('SITE_PORT')
-SSL_ENABLED = env('SSL_ENABLED')
+
